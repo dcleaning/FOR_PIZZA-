6dbb1629261a8e3a7c6613d838e97199c241f036
Daniil8642 <dcleaning.contact@gmail.com>
2024-10-02 11:25:49 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+function setLanguage(language) {
+    if (language === 'ua') {
+      // Логика для переключения на украинский
+      document.querySelector('.lang-switch').textContent = 'Українська';
+      // Здесь вы можете добавить больше логики для смены текста на странице
+    } else if (language === 'ru') {
+      // Логика для переключения на русский
+      document.querySelector('.lang-switch').textContent = 'Русский';
+      // Здесь вы можете добавить больше логики для смены текста на странице
+    }
+  }
+  
